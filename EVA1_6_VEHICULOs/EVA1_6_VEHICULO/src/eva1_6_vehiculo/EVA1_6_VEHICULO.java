42b73e19cfbcd383d4817b5a4c6ddf03679df909
lMax315l <L26550264@chihuahua2.TECNM.MX>
2026-09-12 01:11:47 -0600

    Add files via upload

---
+++ EVA1_6_VEHICULOs/EVA1_6_VEHICULO/src/eva1_6_vehiculo/EVA1_6_VEHICULO.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_vehiculo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_6_VEHICULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String marca;
+        String modelo;
+        int ano;
+        String color;
+        int km;
+        double precio;
+        Scanner capt = new Scanner(System.in);
+        
+        System.out.println("Captura La Marca:");
+        marca = capt.nextLine();
+        System.out.println("Captura el Modelo:");
+        modelo = capt.nextLine();
+        System.out.println("Captura el Año:");
+        ano = capt.nextInt();
+        System.out.println("Captura el Color:");
+        color = capt.nextLine();
+        System.out.println("Captura el kilometraje:");
+        km = capt.nextInt();
+        System.out.println("Captura el precio:");
+        precio = capt.nextDouble();
+        
+        System.out.println("Datos Capturados:");
+        System.out.print("Marca:");
+        System.out.println(marca);
+        System.out.print("Modelo:");
+        System.out.println(modelo);
+        System.out.print("Año:");
+        System.out.println(ano);
+        System.out.print("Color:");
+        System.out.println(color);
+        System.out.print("Kilometraje:");
+        System.out.println(km);
+        System.out.print("Precio:");
+        System.out.println(precio);
+        
+        
+    }
+    
+}
